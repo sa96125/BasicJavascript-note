ad82b19b918ecf870cb3f84d6bd6dcb8c32f0b2c
sa96125 <sa96125@gmail.com>
2021-02-08 23:47:54 +0900

    2021-02-09

---
+++ DoublyLinkedList.js
@@ -0,0 +1,18 @@
+// 베이스는 싱글리링크드리스트와 같다.
+// 더이상 리버스로 고통 받지 않아도 된다 ㅎㅎ
+// 차이점은 오직 pre 포인터가 추가 되었다는 점.
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.pre = null;
+        this.next = null;
+    }
+}
+
+class SingleList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = null;
+    }
