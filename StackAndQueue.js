c2e41cbce0dd30be831ccc4831e693b63261b77c
sa96125 <sa96125@gmail.com>
2021-02-09 19:14:58 +0900

    2021-02-09

--- StackAndQueue.js
+++ StackAndQueue.js
@@ -1,4 +1,8 @@
-// stack
+// stack : 데이터의 집합이다 (LIFO)
+// 정해진 순서가 있는 그냥 컨셉일 뿐이다.
+// 자바스크립트에는 빌트인 스택이 없기 때문에 배열을 사용한다.
+// 하지만 배열의 리 인덱싱의 단점이 있기때문에 스택을 구현하기 위해서는 링크드리스트를 사용하여 자체적으로 코드를 짜야한다.
+
 class Node {
     constructor(val) {
         this.val = val;
@@ -26,5 +30,15 @@ class SingleList {
         return ++this.size;
     }
 
-
+    pop() {
+        if (!this.size) return undefined;
+        if (this.size === 1) {
+            this.first = null;
+            this.last = null;
+        } else {
+            this.first = this.first.next;
+            this.size--;
+        }
+        return temp.val;
+    }
 }
