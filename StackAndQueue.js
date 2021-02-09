300f660d1a3752291249fdd6fcfab91998679aad
sa96125 <sa96125@gmail.com>
2021-02-09 19:18:56 +0900

    2021-02-09

--- StackAndQueue.js
+++ StackAndQueue.js
@@ -42,3 +42,46 @@ class SingleList {
         return temp.val;
     }
 }
+
+// queue : 데이터의 집합이다 (FIFO)
+// 스택과 마찬가지로 정해진 순서가 있는 그냥 컨셉일 뿐이다. 하지만 다른 순서를 가진다.
+// 자바스크립트에는 빌트인 스택이 없기 때문에 배열을 사용한다.
+// 하지만 배열의 리 인덱싱의 단점이 있기때문에 스택을 구현하기 위해서는 링크드리스트를 사용하여 자체적으로 코드를 짜야한다.
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SingleList {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = null;
+    }
+
+    enqueue(val) {
+        const newnode = new Node(val);
+        if (!this.size) {
+            this.first = newnode;
+            this.last = newnode;
+        } else {
+            this.last.next = newnode;
+            this.last = newnode;
+        }
+        return ++this.size;
+    }
+
+    dequeue() {
+        if (!this.first) return null;
+        var temp = this.first;
+        if (this.first === this.last) {
+            this.last = null;
+        }
+        thhis.first = temp.next;
+        this.size--;
+        return temp.val;
+    }
+}
+
