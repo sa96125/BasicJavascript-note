3608f8d1b0f025b354c5f79d3e96e58905c60540
sa96125 <sa96125@gmail.com>
2021-02-15 22:16:22 +0900

    2021-02-15

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -1,13 +1,86 @@
+// 노드는 최대 2개의 자식노드를 갖는다.
 class Node {
-    constructor(val) {
-        this.val = val;
+    constructor(value) {
+        this.value = value;
         this.left = null;
         this.right = null;
     }
 }
 
-class BST {
+// 이분탐색트리
+// 음~~ 개인적으로 삽입정렬의 비선형구조로서의 업그레이드판이라고 생각하면 될듯.
+// 각각의 노드에 비교를 하여 작은 값은 왼쪽으로 큰값은 오른쪽으로 이동하여 저장.
+// 원하는 데이터를 찾기 쉽고 빠르다.
+class BinarySearchTree {
     constructor() {
         this.root = null;
     }
+    insert(value) {
+        var newNode = new Node(value);
+        if (this.root === null) {
+            this.root = newNode;
+            return this;
+        }
+
+        // 비교할 대상이 지속적으로 바껴야 하므로 포인터를 하나 지정한다.
+        var current = this.root;
+        while (true) {
+            if (value === current.value) return undefined;
+            if (value < current.value) {
+                if (current.left === null) {
+                    current.left = newNode;
+                    return this;
+                }
+                current = current.left;
+            } else {
+                if (current.right === null) {
+                    current.right = newNode;
+                    return this;
+                }
+                current = current.right;
+            }
+        }
+    }
+    find(value) {
+        if (this.root === null) return false;
+        var current = this.root,
+            found = false;
+        while (current && !found) {
+            if (value < current.value) {
+                current = current.left;
+            } else if (value > current.value) {
+                current = current.right;
+            } else {
+                found = true;
+            }
+        }
+        if (!found) return undefined;
+        return current;
+    }
+
+    contains(value) {
+        if (this.root === null) return false;
+        var current = this.root,
+            found = false;
+        while (current && !found) {
+            if (value < current.value) {
+                current = current.left;
+            } else if (value > current.value) {
+                current = current.right;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
 }
+
+
+var tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
+tree.BFS();
