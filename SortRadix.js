0665d28b3c581cf57cb97cedbeda90115a98767f
sa96125 <sa96125@gmail.com>
2021-02-21 22:38:40 +0900

    2021-02-21

--- SortRadix.js
+++ SortRadix.js
@@ -1,12 +1,18 @@
+// 레딕스 정렬은 오직 인티져값에만 적용되는 아주 빠른 정렬방법이다.
+// 숫자와 자릿수의 특성을 이용한 정렬이다.
+
+// 비교할 숫자를 가져옴.
 function getDisit(num, i) {
     return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
 }
 
+// 전달되는 숫자가 현재 몇번째 자릿수를 가지는가를 얻어냄.
 function disitCount(num) {
     if (num === 0) return 1;
     return Math.floor(Math.log10(Math.abs(num))) + 1;
 }
 
+// 모든 숫자들을 비교해서 가장 자릿수가 큰값을 가져온다.
 function mostDisit(nums) {
     let maxdisits = 0;
     for (let i = 0; nums.length; i++) {
